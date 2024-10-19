d2399cfacf986426b4dce6adba1ea37b6b810879
pgvalle <pgvalle.nogueira@gmail.com>
2024-10-19 20:02:56 -0300

    Precompiled headers done right

---
+++ src/common.h
@@ -0,0 +1,13 @@
+#pragma once
+
+
+#include <cstdio>
+#include <ctime>
+#include <array>
+#include <string>
+#include <functional>
+#include <unordered_map>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_FontCache.h>
