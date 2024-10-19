c0bd3ce40af34db17125e7f43cdf6620fc653a7b
pgvalle <pgvalle.nogueira@gmail.com>
2024-10-19 14:50:46 -0300

    Removing build directory

--- build/CMakeFiles/rgf.dir/src/hook.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/rgf.dir/src/hook.cpp.o: /home/pedro/Projects/RGF/src/hook.cpp \
- /usr/include/stdc-predef.h /home/pedro/Projects/RGF/src/game.h \
- /home/pedro/Projects/RGF/src/common.h
--- build/CMakeFiles/rgf.dir/src/start.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/rgf.dir/src/start.cpp.o: \
- /home/pedro/Projects/RGF/src/start.cpp /usr/include/stdc-predef.h \
- /home/pedro/Projects/RGF/src/game.h \
- /home/pedro/Projects/RGF/src/common.h
